63b71131e646595517ab0a0a8e71d1af721cb365
lscereyes <lsc.ereyes@gmail.com>
2017-03-27 12:53:57 -0600

    1er Commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=GSurvey',
+    'username' => 'root',
+    'password' => '$7Yrd87eR',
+    'charset' => 'utf8',
+];
---
+++ models/ImagenForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+
+class ImagenForm extends Model{
+    public $foto;
+
+    public function rules()
+    {
+        return array(
+           array('foto','file','types'=>'jpg, jpeg, png, gif')
+            );
+    }
+
+    public function attributeLabels()
+	{
+		return array(
+                        'foto'=>'Foto',
+		);
+	}
+
+}
+?>
